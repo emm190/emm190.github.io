2fc2ddbcf4985e40df11ba8f9f8a33ddc428d07c
emm190 <emm190@pitt.edu>
2021-12-07 12:02:55 -0500

    added positive and negative feedback headers

--- app.js
+++ app.js
@@ -279,7 +279,8 @@ function showFeedbackList(n){
     var rowCount = 0;
     //if all are checked display correct answer feedback 
     if(n[0].checked && n[3].checked && n[6].checked && n[9].checked && n[12].checked) {
-        document.getElementById("feedbacklist").innerHTML = '<li>During the primary survey, you correctly identified that this patient had a life-threatening partial amputation and placed a tourniquet on the proximal right leg to stop the bleeding. Good job.</li>'
+        document.getElementById("positiveFeedBackList").innerHTML = '<li>During the primary survey, you correctly identified that this patient had a life-threatening partial amputation and placed a tourniquet on the proximal right leg to stop the bleeding.</li>'
+        document.getElementById("negativeFeedBackList").innerHTML = '<li>You performed everything correctly.</li>'
     }
     else { 
         for(var i = 0; i<n.length; i++)
